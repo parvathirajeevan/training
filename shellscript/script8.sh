81e595733fd673e5173f324725f16ba8172ff27b
EC2 Default User <ec2-user@ip-10-0-0-43.ec2.internal>
2021-02-28 16:43:51 +0000

    test

---
+++ shellscript/script8.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "I'm goinf to print numbers from 1 to 100"
+
+for i in `seq 1 100`
+do
+	echo $i
+done
---
+++ shellscript/script9.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "I'm goinf to print numbers from 1 to 200"
+
+for i in `seq 1 200`
+do
+	echo $i
+done
