bca4e31cbd18c765dc834513f382f451e13e4db7
EC2 Default User <ec2-user@ip-10-0-0-30.ec2.internal>
2021-02-28 23:08:38 +0000

    test

--- prometheus/main.sh
+++ prometheus/main.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 sudo yum update -y
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-source ~/.bashrc
+#source ~/.bashrc
 
 
 ## lets pull docker image for proemethusA
-docker pull gocd/gocd-server
+#docker pull gocd/gocd-server
 
 
 ## now lets run prometheus docker
-docker run \
+sudo docker run -d \
     -p 9090:9090 \
     -v /home/ec2-user/training/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
     prom/prometheus
@@ -20,4 +20,4 @@ docker run \
 
 
 ### install and configure grafana
-docker run -d -p 3000:3000 grafana/grafana
+sudo docker run -d -p 3000:3000 grafana/grafana
